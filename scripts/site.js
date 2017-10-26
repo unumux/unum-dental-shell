7f4d2aa53cc9fe8ae6c21efd38154bad984748f7
Bolling <bball@unum.com>
2017-10-26 13:29:32 -0400

    updates secondary nav functionality

--- scripts/site.js
+++ scripts/site.js
@@ -1,3 +1,12 @@
-// Main JS should go here!
-// Include scripts using Browserify by doing:
-// var $ = require("jquery");
+var header = document.querySelector(".willow-page-header");
+var open = document.querySelector(".willow-page-header__content-open");
+var close = document.querySelector(".willow-page-header__content-close");
+
+open.addEventListener("click", function(e) {
+    e.preventDefault();
+    header.setAttribute("data-content-open", "true");    
+});
+close.addEventListener("click", function(e) {
+    e.preventDefault();
+    header.setAttribute("data-content-open", "false");
+});
